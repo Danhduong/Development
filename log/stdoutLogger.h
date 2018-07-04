80415ea5cd06c70f9f6cd295df4e552eba231db6
Danh Duong <danhduong1511@gmail.com>
2018-07-04 16:21:53 +0000

    Cleanning

--- log/stdoutLogger.h
+++
@@ -1,9 +0,0 @@
-#include "baseLogger.h"
-
-class StdOutLogger : public BaseLogger
-{
-    public :
-        StdOutLogger(void);
-        ~StdOutLogger(void);
-        void Log(LOG_LEVEL level, const char * strMsg, const char * file, const unsigned long line, const char * function);
-};
